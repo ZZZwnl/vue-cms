3b7a929101208a026d78e9b88f2b52a5fb140337
ssh68 <610353134@qq.com>
2019-05-07 14:45:25 +0800

    搭建-header-tarbar

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 引入mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+// 引入MUI的样式
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
+Vue.use(MintUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
